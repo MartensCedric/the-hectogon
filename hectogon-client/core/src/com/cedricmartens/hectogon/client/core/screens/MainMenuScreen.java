3202a48b1d8ad41a91b4bbe26eda303e420e292f
MartensCedric <cedricmartens98@gmail.com>
2017-11-08 21:11:14 -0500

    Added bubble chat skin

--- hectogon-client/core/src/com/cedricmartens/hectogon/client/core/screens/MainMenuScreen.java
+++ hectogon-client/core/src/com/cedricmartens/hectogon/client/core/screens/MainMenuScreen.java
@@ -3,6 +3,8 @@ package com.cedricmartens.hectogon.client.core.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 public class MainMenuScreen extends StageScreen
 {
@@ -17,6 +19,10 @@ public class MainMenuScreen extends StageScreen
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Skin skin = new Skin(Gdx.files.internal("skins/message.json"));
+        Label label = new Label("Hey guys it's Maxmoefoe", skin);
+        getStage().addActor(label);
+
         super.render(delta);
     }
 
--- hectogon-client/desktop/src/com/cedricmartens/hectogon/client/desktop/DesktopLauncher.java
+++ hectogon-client/desktop/src/com/cedricmartens/hectogon/client/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import com.cedricmartens.hectogon.client.core.Hectogon;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 960;
-		config.height = 540;
+		config.width = 1280;
+		config.height = 720;
 		new LwjglApplication(new Hectogon(), config);
 	}
 }
