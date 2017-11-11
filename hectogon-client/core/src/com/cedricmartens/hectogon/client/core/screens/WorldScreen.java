40a07b488d64418d088d83ec45db451b4546f905
MartensCedric <cedricmartens98@gmail.com>
2017-11-11 08:42:30 -0500

    added chat input

--- hectogon-client/core/src/com/cedricmartens/hectogon/client/core/screens/WorldScreen.java
+++ hectogon-client/core/src/com/cedricmartens/hectogon/client/core/screens/WorldScreen.java
@@ -2,13 +2,19 @@ package com.cedricmartens.hectogon.client.core.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.cedricmartens.hectogon.client.core.game.SceneManager;
+import com.cedricmartens.hectogon.client.core.ui.UiUtil;
+
 
 public class WorldScreen extends StageScreen{
 
     public WorldScreen(SceneManager sceneManager)
     {
         super(sceneManager);
+        TextField chatInput = new TextField("", UiUtil.getChatSkin());
+        chatInput.setWidth(400);
+        getStage().addActor(chatInput);
     }
 
     @Override
--- hectogon-client/core/src/com/cedricmartens/hectogon/client/core/ui/UiUtil.java
+++ hectogon-client/core/src/com/cedricmartens/hectogon/client/core/ui/UiUtil.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 public class UiUtil
 {
     private static Skin defaultSkin;
+    private static Skin chatSkin;
+
     public static Skin getDefaultSkin()
     {
         if(defaultSkin == null)
@@ -13,5 +15,15 @@ public class UiUtil
             defaultSkin = new Skin(Gdx.files.internal("skins/default/skin/uiskin.json"));
         }
         return defaultSkin;
+    }
+
+    public static Skin getChatSkin()
+    {
+        if(chatSkin == null)
+        {
+            chatSkin = new Skin(Gdx.files.internal("skins/chat/message.json"));
+        }
+
+        return chatSkin;
     }
 }
