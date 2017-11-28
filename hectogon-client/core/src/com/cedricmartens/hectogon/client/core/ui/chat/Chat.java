c1dd8e7e33e3338f52ad1415d07e97d5d2a43602
MartensCedric <cedricmartens98@gmail.com>
2017-11-28 10:24:04 -0500

    can now open and close chat

--- hectogon-client/core/src/com/cedricmartens/hectogon/client/core/ui/chat/Chat.java
+++ hectogon-client/core/src/com/cedricmartens/hectogon/client/core/ui/chat/Chat.java
@@ -1,9 +1,14 @@
 package com.cedricmartens.hectogon.client.core.ui.chat;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import com.cedricmartens.commons.chat.Message;
 
@@ -11,6 +16,9 @@ public class Chat extends ScrollPane
 {
     private Table messageTable;
     private ChatStyle chatStyle;
+    private boolean isOpen;
+    private ImageButton toggleWindow;
+    private boolean finishInit = false;
 
     public Chat(Skin skin)
     {
@@ -18,6 +26,63 @@ public class Chat extends ScrollPane
         this.chatStyle = skin.get(ChatStyle.class);
         this.messageTable = (Table) getActor();
         this.setScrollingDisabled(true, false);
+        this.isOpen = true;
+
+        this.toggleWindow = new ImageButton(chatStyle.closeChat, chatStyle.closeChatDown);
+        this.toggleWindow.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                isOpen = !isOpen;
+                sizeChanged();
+                if(isOpen)
+                {
+                    ImageButton.ImageButtonStyle style = new ImageButton.ImageButtonStyle();
+                    style.up = chatStyle.closeChat;
+                    style.over = chatStyle.closeChatDown;
+                    toggleWindow.setStyle(style);
+                }else{
+                    ImageButton.ImageButtonStyle style = new ImageButton.ImageButtonStyle();
+                    style.up = chatStyle.openChat;
+                    style.over = chatStyle.openChatDown;
+                    toggleWindow.setStyle(style);
+                }
+            }
+        });
+
+        finishInit = true;
+    }
+
+    @Override
+    public float getHeight() {
+        if(!isOpen)
+            return 0;
+        return super.getHeight();
+    }
+
+    @Override
+    protected void positionChanged() {
+        super.positionChanged();
+        if(finishInit)
+            updateUI();
+    }
+
+    @Override
+    protected void sizeChanged() {
+        super.sizeChanged();
+        if(finishInit)
+            updateUI();
+    }
+
+    private void updateUI()
+    {
+        this.toggleWindow.setX(getWidth() + getX() - 8);
+        this.toggleWindow.setY(getHeight() + getY() - 8);
+    }
+
+    @Override
+    protected void setStage(Stage stage) {
+        super.setStage(stage);
+        stage.addActor(toggleWindow);
     }
 
     public void addMessage(Message message)
@@ -28,28 +93,21 @@ public class Chat extends ScrollPane
         this.scrollTo(0, 0, 0, 0);
     }
 
-    @Override
-    public void draw(Batch batch, float parentAlpha) {
-        super.draw(batch, parentAlpha);
-        //this.chatStyle.openChat.draw(batch, 0,0, 8,8);
-    }
-
     static public class ChatStyle extends ScrollPane.ScrollPaneStyle
     {
         public Drawable openChat;
-        public Drawable closeChat;
+        public Drawable openChatDown;
 
-        public ChatStyle(Drawable openChat, Drawable closeChat, ScrollPaneStyle scrollPaneStyle)
-        {
-            super(scrollPaneStyle);
-            this.openChat = openChat;
-            this.closeChat = closeChat;
-        }
+        public Drawable closeChat;
+        public Drawable closeChatDown;
 
         public ChatStyle(ChatStyle chatStyle)
         {
             this.openChat = chatStyle.openChat;
+            this.openChatDown = chatStyle.openChatDown;
+
             this.closeChat = chatStyle.closeChat;
+            this.closeChatDown = chatStyle.closeChatDown;
         }
 
         public ChatStyle()
